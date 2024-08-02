514f949c8cd11b1d79e867620bd34c60b36c692d
Diego Alonso Molina <diegusnueva@gmail.com>
2024-08-02 11:15:07 +0200

    Componentes del PRODUCTO creados: cart-item-detail, order-list, product-catalog

---
+++ src/app/product/components/cart-item-detail/cart-item-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item-detail',
+  templateUrl: './cart-item-detail.component.html',
+  styleUrl: './cart-item-detail.component.css'
+})
+export class CartItemDetailComponent {
+
+}
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
+import { CartItemDetailComponent } from './components/cart-item-detail/cart-item-detail.component';
+import { OrderListComponent } from './components/order-list/order-list.component';
+import { ProductCatalogComponent } from './components/product-catalog/product-catalog.component';
 
 
 @NgModule({
   declarations: [
-    ProductComponent
+    ProductComponent,
+    CartItemDetailComponent,
+    OrderListComponent,
+    ProductCatalogComponent
   ],
   imports: [
     CommonModule,
