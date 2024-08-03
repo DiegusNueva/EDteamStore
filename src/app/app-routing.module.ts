a682fb3ad3184cd75ae1dbac7a0f660c26621e89
Diego Alonso Molina <diegusnueva@gmail.com>
2024-08-03 13:13:35 +0200

    Rutas del propio componente de USER, más las rutas padre del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { authGuard } from './user/helpers/admin.guard';
 
 const routes: Routes = [
   {
