82f52647abf171a3752e0e7d47c8b7d07c65864b
Diego Alonso Molina <diegusnueva@gmail.com>
2024-08-03 11:56:31 +0200

    Lógica relativa al LOGIN

---
+++ src/app/user/interfaces/login-response.interface.ts
@@ -0,0 +1,9 @@
+export interface LoginResponse {
+    message: string;
+    user: {
+        id: number;
+        firstName: string;
+        lastName: string;
+        email: string;
+    }
+}
---
+++ src/app/user/interfaces/sigup-response.interface.ts
@@ -0,0 +1,6 @@
+export interface SignUpResponse {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
