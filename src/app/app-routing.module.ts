db1b7e49bc31d62c931d6c810653db06bddc35a0
Diego Alonso Molina <diegusnueva@gmail.com>
2024-08-03 13:08:09 +0200

    Desarrollamos el helper en USER y las posibles rutas del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }, { path: 'user', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  {
+    path: 'product',
+    loadChildren: () =>
+      import('./product/product.module').then((m) => m.ProductModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
