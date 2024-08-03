f4c11ef646f592dd30c5a0e2db3c3f49227236dd
Diego Alonso Molina <diegusnueva@gmail.com>
2024-08-03 12:36:02 +0200

    Componente HOME

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../user/services/auth.service';
+import { Router } from '@angular/router';
+import { LoginResponse } from '../user/interfaces/login-response.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  userFullName: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    const loginResponse: LoginResponse | null =
+      this.authService.getLoginResponse();
+
+    // Verificar si el usuario ha iniciado sesión y obtener el nombre completo si es así
+    if (loginResponse && this.authService.isLoggedIn()) {
+      const { firstName, lastName } = loginResponse.user;
+      this.userFullName = `${firstName} ${lastName}`;
+    }
+  }
 }
