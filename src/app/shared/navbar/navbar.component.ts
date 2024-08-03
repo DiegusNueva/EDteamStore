fbe0d839768abca4f77eb208172be03256b628fd
Diego Alonso Molina <diegusnueva@gmail.com>
2024-08-03 12:32:55 +0200

    Componente NAVBAR

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../user/services/auth.service';
+
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['']);
+  }
+
+  isLoggedIn(): boolean {    
+    return this.authService.isLoggedIn();
+  }
+
+ 
 }
